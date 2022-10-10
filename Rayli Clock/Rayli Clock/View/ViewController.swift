a5fc7909a34b54bd8feed4cbd980a8565a4a29f3
Emmanuel Mensah <immaxp183@gmail.com>
2022-10-10 09:46:38 +0200

    Added image, label and after i  linked it to the view controller

--- Rayli Clock/Rayli Clock/View/ViewController.swift
+++ Rayli Clock/Rayli Clock/View/ViewController.swift
@@ -8,12 +8,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    //Properties
+   @IBOutlet weak var clockLabel: UILabel!
+    var timer = Timer()
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        timer = Timer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
     }
 
+    @objc func updateTime(){
+        let timeFormatter = DateFormatter()
+    }
 
 }
 
