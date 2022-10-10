0b00874c98466b4c37060386e63988049e19f451
Emmanuel Mensah <immaxp183@gmail.com>
2022-10-10 14:35:17 +0200

    Added a clock function

--- Rayli Clock/Rayli Clock/View/ViewController.swift
+++ Rayli Clock/Rayli Clock/View/ViewController.swift
@@ -10,15 +10,22 @@ import UIKit
 class ViewController: UIViewController {
     //Properties
    @IBOutlet weak var clockLabel: UILabel!
-    var timer = Timer()
+   var timer = Timer()
    
     override func viewDidLoad() {
         super.viewDidLoad()
-        timer = Timer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTime), userInfo: nil, repeats: true)
+        self.updateTime()
     }
 
+   
+    
     @objc func updateTime(){
+       
         let timeFormatter = DateFormatter()
+        timeFormatter.timeStyle = .medium
+        clockLabel.text = timeFormatter.string(from: NSDate() as Date)
+        
     }
 
 }
