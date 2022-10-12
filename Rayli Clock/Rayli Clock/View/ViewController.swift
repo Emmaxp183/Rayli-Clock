f0f9d4bc2edde15104a561a4b237573b918716dc
Emmanuel Mensah <immaxp183@gmail.com>
2022-10-12 11:12:10 +0200

    Added functions to change the actions of the app and renamed my photos

--- Rayli Clock/Rayli Clock/View/ViewController.swift
+++ Rayli Clock/Rayli Clock/View/ViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     //Properties
+    @IBOutlet weak var backgroundImage: UIImageView!
     @IBOutlet weak var settingsView: UIView!
     @IBOutlet weak var settingsButton: UIButton!
     @IBOutlet weak var changeFontSeg: UISegmentedControl!
@@ -40,20 +41,47 @@ class ViewController: UIViewController {
     
     
     @IBAction func backgroundImageSeg(_ sender: UISegmentedControl) {
+        if(changeBackgroundSeg.selectedSegmentIndex == 0){
+            backgroundImage.image = UIImage(named: "")
+        }
+        if(changeBackgroundSeg.selectedSegmentIndex == 1){
+            backgroundImage.image = UIImage(named: "photo1")
+        }
+        if(changeBackgroundSeg.selectedSegmentIndex == 2){
+            backgroundImage.image = UIImage(named: "photo2")
+        }
+        if(changeBackgroundSeg.selectedSegmentIndex == 3){
+            backgroundImage.image = UIImage(named: "photo3")
+        }
     }
    
     @IBAction func showSettingsButton(_ sender: UIButton) {
         if settingsView.isHidden == true {
             settingsView.isHidden = false
             settingsButton.setTitle("Hide Settings", for: UIControl.State.normal)
+            settingsButton.alpha = 1.0
         }else{
             settingsView.isHidden = true
             settingsButton.setTitle("Show Settings", for: UIControl.State.normal)
+            settingsButton.alpha = 0.3
         }
     }
     
     
     @IBAction func clockFontSeg(_ sender: UISegmentedControl) {
+        
+        if (changeFontSeg.selectedSegmentIndex == 0){
+            clockLabel.font = UIFont(name: "Digital Numbers", size: 36)
+        }
+        if (changeFontSeg.selectedSegmentIndex == 1){
+            clockLabel.font = UIFont(name: "New Waltograph", size: 36)
+        }
+        if (changeFontSeg.selectedSegmentIndex == 2){
+            clockLabel.font = UIFont(name: "Orbitron", size: 36)
+        }
+        if (changeFontSeg.selectedSegmentIndex == 3){
+            clockLabel.font = UIFont(name: "Avenir", size: 36)
+        }
     }
 }
 
